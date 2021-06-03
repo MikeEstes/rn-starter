8870232fc09b729a387dc324cb4d6af8d33d07c5
Mike Estes <sregge@gmail.com>
2021-06-03 13:05:38 -0400

    Lesson 2

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,31 @@
+/* 4 steps to creating a component */
+// 1. Import Libraries
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+// 2. Create a Component (A function that returns some JSX)
+// const ComponentsScreen = function() { (These are the same thing)
+const ComponentsScreen = () => {
+	// JS Variable
+	const name = "Mike";
+
+	return (
+		<View>
+			<Text style={styles.headerStyle}>Getting Started with React Native!</Text>
+			<Text style={styles.subHeaderStyle}>My name is {name}</Text>
+		</View>
+	);
+};
+
+// 3. Create a Stylesheet
+const styles = StyleSheet.create({
+	headerStyle: {
+		fontSize: 45
+	},
+	subHeaderStyle: {
+		fontSize: 20
+	}
+});
+
+// 4. Export Component
+export default ComponentsScreen;
