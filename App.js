df21bee21bced71d5a99401840f952cd9b7ad7b8
Mike Estes <sregge@gmail.com>
2021-06-04 01:39:25 -0400

    States!

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import SquareScreen from './src/screens/SquareScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen';
+import CounterScreen from './src/screens/CounterScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import ImageScreen from './src/screens/ImageScreen';
 import ListScreen from './src/screens/ListScreen';
+import ColorScreen from './src/screens/ColorScreen';
 
 const navigator = createStackNavigator(
   {
+    Color: ColorScreen,
     Components: ComponentsScreen,
+    Counter: CounterScreen,
     Home: HomeScreen,
-    List: ListScreen,
     Image: ImageScreen,
+    List: ListScreen,
+    Square: SquareScreen,
   },
   {
     initialRouteName: 'Home',
